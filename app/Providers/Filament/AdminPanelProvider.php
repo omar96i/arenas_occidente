e1b90bc14eb67302249ccaf76a50d6bbf73ffd6a
omar96i <sagara9930@hotmail.com>
2023-10-17 09:29:25 -0500

    update logo

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -27,6 +27,7 @@ class AdminPanelProvider extends PanelProvider
             ->id('admin')
             ->path('admin')
             ->login()
+            ->brandLogo(asset('icono.png'))
             ->colors([
                 'primary' => Color::Amber,
             ])
