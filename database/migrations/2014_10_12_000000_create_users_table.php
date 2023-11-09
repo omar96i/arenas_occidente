b137faa092937b4b7be8711ae48f97d6b8dc4739
KeniierM <keniier.321@gmail.com>
2023-11-09 08:01:53 -0500

    fix(User) personal information en user

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,6 +18,10 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('full_name')->nullable();
+            $table->string('last_name')->nullable();
+            $table->string('document')->nullable();
+            $table->string('address')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
